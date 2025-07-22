4880a0571600fb7bda6744c509d34ccec6ca8242
Billal Ghilas <84322223+gBillal@users.noreply.github.com>
2025-07-22 23:24:40 +0100

    Improve parsing of hdlr and dref boxes (#33)

--- lib/ex_mp4/box/dinf.ex
+++ lib/ex_mp4/box/dinf.ex
@@ -5,18 +5,17 @@ defmodule ExMP4.Box.Dinf do
   The data information box contains objects that declare the location of the media information in a track
   """
 
+  import ExMP4.Box.Utils, only: [parse_header: 1]
+
+  alias ExMP4.Box.DataEntryURL
+
   @type t :: %__MODULE__{dref: map()}
 
   defstruct dref: %{
               version: 0,
               flags: 0,
               entry_count: 1,
-              data_entry: [
-                %{
-                  version: 0,
-                  flags: 1
-                }
-              ]
+              data_entry: [%DataEntryURL{version: 0, flags: 1}]
             }
 
   defimpl ExMP4.Box do
@@ -24,18 +23,14 @@ defmodule ExMP4.Box.Dinf do
       ExMP4.header_size() + ExMP4.full_box_header_size() * 2 + 4
     end
 
-    def parse(
-          box,
-          <<_size::32, "dref", version::8, flags::24, entry_count::32, _url_size::32, "url ",
-            url_version::8, url_flags::24>>
-        ) do
+    def parse(box, <<_size::32, "dref", version::8, flags::24, entry_count::32, rest::binary>>) do
       %{
         box
         | dref: %{
             version: version,
             flags: flags,
             entry_count: entry_count,
-            data_entry: [%{version: url_version, flags: url_flags}]
+            data_entry: parse_entries(rest)
           }
       }
     end
@@ -44,8 +39,20 @@ defmodule ExMP4.Box.Dinf do
       [
         <<size(box)::32, "dinf", 28::32, "dref", box.dref.version::8, box.dref.flags::24,
           box.dref.entry_count::32>>,
-        Enum.map(box.dref.data_entry, &<<12::32, "url ", &1.version::8, &1.flags::24>>)
+        Enum.map(box.dref.data_entry, &ExMP4.Box.serialize/1)
       ]
+    end
+
+    defp parse_entries(<<>>), do: []
+
+    defp parse_entries(rest) do
+      case parse_header(rest) do
+        {"url ", box_data, rest} ->
+          [ExMP4.Box.parse(%DataEntryURL{}, box_data) | parse_entries(rest)]
+
+        {name, box_data, rest} ->
+          parse_entries(rest)
+      end
     end
   end
 end
--- lib/ex_mp4/box/hdlr.ex
+++ lib/ex_mp4/box/hdlr.ex
@@ -18,7 +18,7 @@ defmodule ExMP4.Box.Hdlr do
   defimpl ExMP4.Box do
     def size(box), do: ExMP4.full_box_header_size() + byte_size(box.name) + 21
 
-    def parse(box, <<version::8, flags::24, 0::32, type::binary-size(4), 0::32*3, name::binary>>) do
+    def parse(box, <<version::8, flags::24, _::32, type::binary-size(4), 0::32*3, name::binary>>) do
       %{
         box
         | version: version,
